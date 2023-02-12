6f5079770b7152bfea735a63d586f971bb80e714
shaderko <shader228@gmail.com>
2023-02-12 23:02:01 +0100

    render working

--- shadegin/build.bat
+++ shadegin/build.bat
@@ -1,4 +1,4 @@
-set files=src\glad.c src\main.c
+set files=src\glad.c src\main.c src\engine\global.c src\engine\render\render_init.c src\engine\render\render.c
 @REM FULL PATH TO PROJECT
 set libs=F:\projects\shadegin\Shadegin\lib\SDL2main.lib F:\projects\shadegin\Shadegin\lib\SDL2.lib F:\projects\shadegin\Shadegin\lib\freetype.lib
 
---
+++ shadegin/src/engine/types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <inttypes.h>
+
+typedef uint32_t u32;
+typedef float f32;
+typedef double f64;
+
+#endif
